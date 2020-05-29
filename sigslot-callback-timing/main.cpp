48f09091df7bde1af5de6837460da3034371668c
AJ <andrew.mcfarlane@ucalgary.ca>
2020-05-28 19:11:15 -0700

    finished prelim demo, hypothesis proven; follow-up to come.

---
+++ sigslot-callback-timing/main.cpp
@@ -0,0 +1,29 @@
+#include <boost/signals2.hpp>
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+struct DemoSlot
+{
+    std::__1::chrono::steady_clock::time_point operator()()
+    {
+        auto beforeSleep = std::chrono::high_resolution_clock::now(); // timestamp BEFORE the callback simulated computation.
+        std::this_thread::sleep_for(std::chrono::seconds(2));         // simulating computation ...
+        return beforeSleep;
+    }
+};
+
+int main()
+{
+    std::cout << "beginning sigslot-callback-timing demo ...\n";
+    boost::signals2::signal<std::__1::chrono::steady_clock::time_point()> sig;
+    DemoSlot slot;
+    sig.connect(slot);
+    auto beforeSig = std::chrono::high_resolution_clock::now(); // timestamp BEFORE the signal
+    auto beforeSlot = sig().get();
+    auto afterSig = std::chrono::high_resolution_clock::now(); // timestamp AFTER the callback trigger
+    std::chrono::duration<double, std::milli> d1 = beforeSlot - beforeSig, d2 = afterSig - beforeSig;
+    std::cout << "\n\ntime between BeforeSig and BeforeSlot: " << d1.count() << " ms.\n";
+    std::cout << "time between BeforeSig and AfterSig: " << d2.count() << " ms.\n";
+    return 0;
+}
